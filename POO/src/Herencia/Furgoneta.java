7b8aef89adae0f2983a88b20af14e7adcfcc8e08
Kotaruo <125087415+Kotaruo@users.noreply.github.com>
2023-04-15 16:13:43 -0500

    Taller de repaso

---
+++ POO/src/Herencia/Furgoneta.java
@@ -0,0 +1,18 @@
+package Herencia;
+
+public class Furgoneta extends Coche{
+
+	private int capacidad_carga;
+	
+	private int plazas_extra;
+	
+	public Furgoneta(int plazas_extra, int capacidad_carga) {
+		super(); // llama al constructor de la clase padre
+		
+		this.capacidad_carga=capacidad_carga;
+		
+		this.plazas_extra=plazas_extra;
+		
+	}
+	
+}
